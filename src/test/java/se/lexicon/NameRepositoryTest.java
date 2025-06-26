26e13aa4378793ba3bc8b953cd234db742098ae7
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-26 15:30:47 +0200

    Added a JUnit test for getSize.

--- src/test/java/se/lexicon/NameRepositoryTest.java
+++ src/test/java/se/lexicon/NameRepositoryTest.java
@@ -1,9 +1,34 @@
 package se.lexicon;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * JUnit 5 tests for the NameRepository class.
  */
 public class NameRepositoryTest {
+
+    @Test
+    void testsGetSizeWithTwoNames() {
+
+        // Arrange: We add two names.
+        NameRepository.setNames(new String[]{"Erik Svensson", "Merdad Javan"});
+
+        // Act: We get the length.
+        int actual = NameRepository.getSize();
+
+        // Assert: We expect the value 2.
+        assertEquals(2, actual, "getSize() should return 2 when two names is added.");
+
+    }
+
+
+
+
+
+
+
 
 }
