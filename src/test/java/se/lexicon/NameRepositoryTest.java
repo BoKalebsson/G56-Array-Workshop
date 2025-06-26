76878fc2d9e735ebf3fb1e956fb6f42e3d2ec114
BoKalebsson <bo_kalebsson@hotmail.com>
2025-06-26 17:13:21 +0200

    Added tests for the findAll-method.

--- src/test/java/se/lexicon/NameRepositoryTest.java
+++ src/test/java/se/lexicon/NameRepositoryTest.java
@@ -15,11 +15,12 @@ public class NameRepositoryTest {
         NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
     }
 
+    // Group: getSize()
     @Test
     void testsGetSize_WithTwoNames() {
 
         // Arrange: We add two names.
-        NameRepository.setNames(new String[]{"Erik Svensson", "Merdad Javan"});
+        NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
 
         // Act: We get the length.
         int actual = NameRepository.getSize();
@@ -40,6 +41,7 @@ public class NameRepositoryTest {
         assertEquals(0, actual, "getSize() should return 0 after clear.");
     }
 
+    // Group: setNames()
     @Test
     void testSetNames_ReplacesNamesCorrectly() {
         // Arrange: Create a new String[] with new names.
@@ -48,7 +50,7 @@ public class NameRepositoryTest {
         // Act: Set the String[].
         NameRepository.setNames(newNames);
 
-        // Assert: Get the String[] and
+        // Assert: Get the String[] and check if the old names have been replaced with the new ones.
         assertArrayEquals(newNames, NameRepository.findAll(), "setNames() should replace all the old names with the new ones.");
     }
 
@@ -61,6 +63,7 @@ public class NameRepositoryTest {
         assertEquals(0, NameRepository.getSize(), "setNames() with an empty String[] should return 0.");
     }
 
+    // Group: clear()
     @Test
     void testClear_ShouldEmptyTheList() {
         // Arrange: We start with our @BeforeEach that enters two names.
@@ -73,5 +76,57 @@ public class NameRepositoryTest {
         assertArrayEquals(new String[0], NameRepository.findAll(), "findAll() shall return an empty String[] after clear().");
     }
 
+    // Group: findAll()
+    @Test
+    void testFindAll_WithTwoNames() {
+        // Arrange: We start with our @BeforeEach that enters two names.
+
+        // Act: We fetch all names through the method we want to test.
+        String[] actualNames = NameRepository.findAll();
+
+        // Assert: We expect that it is the same names as in the String[].
+        assertArrayEquals(
+                new String[]{"Erik Svensson", "Mehrdad Javan"},
+                actualNames,
+                "findAll() should return all names currently stored"
+        );
+    }
+
+    @Test
+    void testFindAll_WhenEmpty() {
+        // Arrange: Empty the String[] with clear().
+        NameRepository.clear();
+
+        // Act: Get all names from the String[].
+        String[] actualNames = NameRepository.findAll();
+
+        // Assert: We expect an empty String[].
+        assertArrayEquals(
+                new String[0],
+                actualNames,
+                "findAll() should return an empty array when the list is cleared"
+        );
+    }
+
+    @Test
+    void testFindAll_ReturnsCopy_NotReference() {
+        // Arrange: We start with our @BeforeEach that enters two names.
+
+        // Act: Get a copy of String[] names.
+        String[] actualNames = NameRepository.findAll();
+
+        // Modify something in the copy.
+        actualNames[0] = "Något annat";
+
+        // Get a copy again to verify that the original has not been changed.
+        String[] namesAfterChange = NameRepository.findAll();
+
+        // Assert: The original should contain the original value.
+        assertEquals("Erik Svensson", namesAfterChange[0], "Changing the returned array should not affect the original data.");
+    }
+
 
 }
+
+
+
